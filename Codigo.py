4f081c7afc88e4c3e4bd5e6692277d0b6757dae0
JosemaUdeM <jose.megadeth@hotmail.com>
2016-10-11 22:09:03 -0500

    funcion extraccion()

--- Codigo.py
+++ Codigo.py
@@ -6,13 +6,13 @@ import requests
 import bs4
 import webbrowser
 
-res = requests.get('http://montcoalert.org/gettingdata/')
-gh = bs4.BeautifulSoup(res.text, "lxml") # tambien es posible desde un archivo abierto
-type(gh)
-tabla_archivos = gh.find_all('div',{'class':'blog-entry-body'})
-type(tabla_archivos)
-len(tabla_archivos)
-for content in tabla_archivos:
-    lineas_a = content('a')
-    if lineas_a:
-        webbrowser.open(lineas_a[1].string)
+def extraccion():
+	res = requests.get('http://montcoalert.org/gettingdata/')
+	gh = bs4.BeautifulSoup(res.text, "lxml") # tambien es posible desde un archivo abierto
+	link = gh.find_all('div',{'class':'blog-entry-body'})
+	for content in link:
+    		lineas_a = content('a')
+    		if lineas_a:
+        		webbrowser.open(lineas_a[1].string)
+
+extraccion()
