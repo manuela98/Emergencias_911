a89b8c2c0161c6c0109fb65a19a97b58c17ef4a5
JosemaUdeM <jose.megadeth@hotmail.com>
2016-11-07 12:03:08 -0500

    agregando funcion Gnuplot

--- Codigo/GenArEst.py
+++ Codigo/GenArEst.py
@@ -291,3 +291,183 @@ def plot():
           
 plot()
 
+def Gnuplot():
+    outfile=open("EMSG.txt","w")
+
+    i=[]
+    Lunes = 0
+    Martes = 0
+    Miercoles = 0
+    Jueves = 0
+    Viernes = 0
+    Sabado = 0
+    Domingo = 0
+    LunesT = 0
+    MartesT = 0
+    MiercolesT = 0
+    JuevesT = 0
+    ViernesT = 0
+    SabadoT = 0
+    DomingoT= 0
+    LunesF = 0
+    MartesF = 0
+    MiercolesF = 0
+    JuevesF = 0
+    ViernesF = 0
+    SabadoF = 0
+    DomingoF = 0
+    Datos =np.loadtxt('texto.txt', dtype = str, delimiter = ';' and ',')
+    necesarios = Datos[1:, [0, 4]]
+    i = 0
+    count2 = [0,]
+    for r in necesarios:
+        if r[0]=="EMS":
+            if r[1] == "Monday":
+                count2 += [1]
+                Lunes += 1
+                i += 1
+            if r[1] == "Tuesday":
+                count2 += [2]
+                Martes += 1
+                i += 1
+            if r[1] == "Wednesday":
+                count2 += [3]
+                Miercoles += 1
+                i += 1
+            if r[1] == "Thursday":
+                Jueves += 1
+                count2 += [4]
+                i += 1
+            if r[1] == "Friday":
+                Viernes += 1
+                count2 += [5]
+                i += 1
+            if r[1] == "Saturday":
+                Sabado += 1
+                count2 += [6]
+                i += 1
+            if r[1] == "Sunday":
+                Domingo += 1
+                count2 += [7]
+                i += 1
+            if count2[i] == 1 and count2[i-1]==7:
+                outfile.write('1 '+str(Lunes)+'\n')
+                outfile.write('2 '+str(Martes)+'\n')
+                outfile.write('3 '+str(Miercoles)+'\n')
+                outfile.write('4 '+str(Jueves)+'\n')
+                outfile.write('5 '+str(Viernes)+'\n')
+                outfile.write('6 '+str(Sabado)+'\n')
+                outfile.write('7 '+str(Domingo)+'\n')
+                Lunes = 0
+                Martes = 0
+                Miercoles = 0
+                Jueves = 0
+                Viernes = 0
+                Sabado = 0
+                Domingo= 0
+          
+
+
+
+    outfile=open("TrafficG.txt","w")
+    j = 0
+    count1 = [0,]
+    for r in necesarios:
+        if r[0]=="Traffic":
+            if r[1] == "Monday":
+                count1 += [1]
+                LunesT += 1
+                j += 1
+            if r[1] == "Tuesday":
+                count1 += [2]
+                MartesT += 1
+                j += 1
+            if r[1] == "Wednesday":
+                count1 += [3]
+                MiercolesT += 1
+                j += 1
+            if r[1] == "Thursday":
+                JuevesT += 1
+                count1 += [4]
+                j += 1
+            if r[1] == "Friday":
+                ViernesT += 1
+                count1 += [5]
+                j += 1
+            if r[1] == "Saturday":
+                SabadoT += 1
+                count1 += [6]
+                j += 1
+            if r[1] == "Sunday":
+                DomingoT += 1
+                count1 += [7]
+                j += 1
+            if count1[j] == 1 and count1[j-1]==7:
+                outfile.write('1 '+str(LunesT)+'\n')
+                outfile.write('2 '+str(MartesT)+'\n')
+                outfile.write('3 '+str(MiercolesT)+'\n')
+                outfile.write('4 '+str(JuevesT)+'\n')
+                outfile.write('5 '+str(ViernesT)+'\n')
+                outfile.write('6 '+str(SabadoT)+'\n')
+                outfile.write('7 '+str(DomingoT)+'\n')
+                LunesT = 0
+                MartesT = 0
+                MiercolesT = 0
+                JuevesT = 0
+                ViernesT = 0
+                SabadoT = 0
+                DomingoT= 0
+          
+    
+    
+    
+
+    outfile=open("FireG.txt","w")
+    i = 0
+    count = [0,]
+    for r in necesarios:
+        if r[0]=="Fire":
+            if r[1] == "Monday":
+                count += [1]
+                LunesF += 1
+                i += 1
+            if r[1] == "Tuesday":
+                count += [2]
+                MartesF += 1
+                i += 1
+            if r[1] == "Wednesday":
+                count += [3]
+                MiercolesF += 1
+                i += 1
+            if r[1] == "Thursday":
+                JuevesF += 1
+                count += [4]
+                i += 1
+            if r[1] == "Friday":
+                ViernesF += 1
+                count += [5]
+                i += 1
+            if r[1] == "Saturday":
+                SabadoF += 1
+                count += [6]
+                i += 1
+            if r[1] == "Sunday":
+                DomingoF += 1
+                count += [7]
+                i += 1
+            if count[i] == 1 and count[i-1]==7:
+                outfile.write('1 '+str(LunesF)+'\n')
+                outfile.write('2 '+str(MartesF)+'\n')
+                outfile.write('3 '+str(MiercolesF)+'\n')
+                outfile.write('4 '+str(JuevesF)+'\n')
+                outfile.write('5 '+str(ViernesF)+'\n')
+                outfile.write('6 '+str(SabadoF)+'\n')
+                outfile.write('7 '+str(DomingoF)+'\n')
+                LunesF = 0
+                MartesF = 0
+                MiercolesF = 0
+                JuevesF = 0
+                ViernesF = 0
+                SabadoF = 0
+                DomingoF= 0
+Gnuplot()
